13552f5a5081f04a2933f842ad9732b2223801c4
Sawbez <63512091+Sawbez@users.noreply.github.com>
2024-03-02 11:42:47 -0800

    fixes

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from sys import argv
 
 from templates import app
 
-if argv[1] == "prod":
+if len(argv) > 1 and argv[1] == "prod":
     app.config.from_object("configurations.ProductionConfig")
 else:
     app.config.from_object("configurations.DevelopmentConfig")
