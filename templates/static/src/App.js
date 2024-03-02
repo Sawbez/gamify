eba01d1f218c7b2a5f7130f7417f1b5ec5ce4a00
Sawbez <63512091+Sawbez@users.noreply.github.com>
2024-03-02 12:09:02 -0800

    stuff

--- templates/static/src/App.js
+++ templates/static/src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./index.css";
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>My React App</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1 className="text-blue">My React App</h1>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
